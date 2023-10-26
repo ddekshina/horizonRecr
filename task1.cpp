97e8d9e170b835691392ab06d6e1904d486e2496
Devi <devidekshina7@gmail.com>
2023-10-26 11:50:55 +0530

    Made a few changes

--- task1.cpp
+++ task1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {   
-    
+    int A[100];
     float a,s=0;char c; float avg; int n;
     cout <<"Enter 1 to start the program\n";
     cin >> n;
@@ -12,26 +12,46 @@ int main()
         {
             cout <<"Enter number: ";
             cin >> a;
+            //To check whether number is repeated
+            A[n-1]=a; 
+            for(int i = 0;i < n; i++)
+            {
+                for (int j = i+1; i < n-i-1; j++)
+                {
+                    if(A[i]==A[j])
+                    {
+                        a=0;
+                        
+                    }
+                }
+                
+            }
+            //Calculation of average
             s=s+a;
             avg=s/(float)n;
             cout <<"Average: "<< avg;
             cout<< "\nEnter y/n to continue/terminate the program\n";
             cin >> c;
-            switch (c)
-            {
-                case 'y':
-                    n++;
-                    break;
-                case 'n':
-                    cout<< "Program terminated";
-                    n=0;
-                    break;
             
-            default:
-                cout<< "\nInvalid Input";
+            if(c!='y'&&c!='n')
+            {
+                cout<< "Invalid Input";
                 break;
             }
-            
+            else
+            {
+                switch (c)
+                {
+                    case 'y':
+                        n++;
+                        break;
+                    case 'n':
+                        cout<< "Program terminated";
+                        n=0;
+                        break;
+                }
+                
+            }
         }
     }
     else
